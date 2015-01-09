88c78d3ff18cf6f69109beca747e013acfb5011a
mchami <mazenchami@mazens-mbp-2.4implus.local>
2015-01-09 14:43:21 -0500

    FoodTracker iOS App

---
+++ FoodTracker/USDAItem.swift
@@ -0,0 +1,27 @@
+//
+//  USDAItem.swift
+//  FoodTracker
+//
+//  Created by Mazen Chami on 1/8/15.
+//  Copyright (c) 2015 Mazen Chami. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc (USDAItem)
+class USDAItem: NSManagedObject {
+
+    @NSManaged var calcium: String
+    @NSManaged var carbohydrate: String
+    @NSManaged var cholesterol: String
+    @NSManaged var dataAdded: NSDate
+    @NSManaged var energy: String
+    @NSManaged var fatTotal: String
+    @NSManaged var idValue: String
+    @NSManaged var name: String
+    @NSManaged var protein: String
+    @NSManaged var sugar: String
+    @NSManaged var vitaminC: String
+
+}
